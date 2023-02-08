8c6321a4b2b6b75508a43dd66405f6f96e0ff5d1
ExMorph <46392432+ExMorph@users.noreply.github.com>
2023-02-08 22:29:55 +0400

    Добавлены try и checked для проерки возможности ввода значений в проверку потери битовых данных

--- Hello Fuzzy Lamp/Program.cs
+++ Hello Fuzzy Lamp/Program.cs
@@ -16,11 +16,13 @@ internal class Program
     {
         Console.WriteLine("Любишь какать?"+'\n'+ "Люби и ж" + '\x48' + "пу подмывать!");
         Console.Write('\n');
-        Console.WriteLine("Строка? \n Ещё одна!");
+        Console.WriteLine("Строка? \nЕщё одна!");
     }
 
     static void CheckOperand()
     {
+
+        Console.WriteLine("\nПроверка операнда");
         int a, b = 2, c;
         //a = b = c;  ERROR - буква C пустая ЁПТА
         a = b = c = 34;
@@ -29,9 +31,31 @@ internal class Program
 
     static void LostData()
     {
+        Console.WriteLine("\nПроверка потери битовых данных");
+
         int a = 33;
-        int b = 255;
-        byte c = (byte)(a + b);
+        int b = 600;
+        Console.WriteLine($"Было в int {a} + {b} = {a+b}");
+
+        a = (byte)a;
+        b = (byte)b;
+        Console.WriteLine($"Стало в byte {a} + {b} = {a + b}");
+
+        byte c = checked((byte)(a + b));
         Console.WriteLine(c);
+
+        try
+        {
+            Console.Write("Введите 1ю цифру для проверки потери битовых данных: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите 2ю цифру для проверки потери битовых данных: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = checked((byte)(a + b));
+            Console.WriteLine(c);
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
