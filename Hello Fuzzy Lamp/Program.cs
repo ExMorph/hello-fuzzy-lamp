16589c887e071a8373dacfe831135858894f6900
ExMorph <46392432+ExMorph@users.noreply.github.com>
2023-02-08 22:08:21 +0400

    Check потеря данных

--- Hello Fuzzy Lamp/Program.cs
+++ Hello Fuzzy Lamp/Program.cs
@@ -8,10 +8,30 @@ internal class Program
         var name = Console.ReadLine();
         Console.WriteLine($"Привет {name}");
         LiteralsCheck();
+        CheckOperand();
+        LostData();
     }
 
     static void LiteralsCheck()
     {
-        Console.WriteLine("Любишь какать?"+'\n'+ "Люби и жопу подмывать?");
+        Console.WriteLine("Любишь какать?"+'\n'+ "Люби и ж" + '\x48' + "пу подмывать!");
+        Console.Write('\n');
+        Console.WriteLine("Строка? \n Ещё одна!");
+    }
+
+    static void CheckOperand()
+    {
+        int a, b = 2, c;
+        //a = b = c;  ERROR - буква C пустая ЁПТА
+        a = b = c = 34;
+        Console.WriteLine(a + " " + b + " " + c);
+    }
+
+    static void LostData()
+    {
+        int a = 33;
+        int b = 255;
+        byte c = (byte)(a + b);
+        Console.WriteLine(c);
     }
 }
