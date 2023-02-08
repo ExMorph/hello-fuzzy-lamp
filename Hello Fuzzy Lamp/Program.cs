ab586163489ab82485999f1418d1a49ae55711cb
ExMorph <46392432+ExMorph@users.noreply.github.com>
2023-02-08 21:09:40 +0400

    Добавил проект

---
+++ Hello Fuzzy Lamp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите свое имя: ");
+        var name = Console.ReadLine();
+        Console.WriteLine($"Привет {name}");
+        LiteralsCheck();
+    }
+
+    static void LiteralsCheck()
+    {
+        Console.WriteLine("Любишь какать?"+'\n'+ "Люби и жопу подмывать?");
+    }
+}
