d48f23965b0b5ca6bae53359d378bcfe32882de6
ExMorph <46392432+ExMorph@users.noreply.github.com>
2023-03-02 01:04:53 +0400

    Try add timer

--- Hello Fuzzy Lamp/Program.cs
+++ Hello Fuzzy Lamp/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System.Reflection.Metadata.Ecma335;
+using System.Timers;
 
 internal class Program
 {
     static int maxIndex;
-    static float timer;
+    static float timerCouldown;
+    static int[,] mas;
+    static int livedPoints;
+
+    private static System.Timers.Timer aTimer;
 
     private static void Main(string[] args)
     {
-        GameLife();
-        //while (AskRestartGame())
-        //    GameLife();
+        GameLifeStart();
     }
 
     public static int ReadLineInt()
@@ -30,12 +33,12 @@ internal class Program
         return checkedNum;
     }
 
-    static void GameLife()
+    static void GameLifeStart()
     {
         Console.WriteLine("Введите размер сетки");
         int massSize = ReadLineInt();
 
-        int[,] mas = new int[massSize, massSize];
+        mas = new int[massSize, massSize];
         maxIndex = massSize - 1;
 
         //Рандомизация значений
@@ -48,16 +51,24 @@ internal class Program
             
         }
 
-        
+        Console.WriteLine("Введите время для обновления данных (сек)");
+        timerCouldown = ReadLineInt()*1000;
+
+        Console.WriteLine("\nВведите S");
         while ("S" != Console.ReadLine())
         {
             Console.WriteLine("\nВведите S");
-            GameLifeCycle(ref mas);
+            GameLifeCycle();
         }
-            
+
+        SetTimer();
+        
+        Console.ReadLine();
+        aTimer.Stop();
+        aTimer.Dispose();
     }
 
-    static void GameLifeCycle(ref int[,] mas)
+    static void GameLifeCycle()
     {
         //Рисование картины
         for (int i = 0; i <= maxIndex; i++)
@@ -67,37 +78,42 @@ internal class Program
             {
                 Console.Write(mas[i, j]);
             }
-
         }
 
         //Подсчет соседей
-        Console.Write('\n');
+        //Console.Write('\n');
         int[,] masTemp = new int[maxIndex + 1, maxIndex + 1];
 
         for (int _x = 0; _x <= maxIndex; _x++)
         {
-            Console.Write('\n');
+            //Console.Write('\n');
             for (int _y = 0; _y <= maxIndex; _y++)
             {
                 int livingHeighbor = CalcHeighbors(mas, _x, _y);
 
                 //Check неживая
-                if (mas[_x, _y] == 0 && livingHeighbor >= 3)
+                if (mas[_x, _y] == 0 && livingHeighbor == 3)
                 {
                     masTemp[_x, _y] = 1;
                 }
                 //Check живая
                 else if (mas[_x, _y] > 0)
                 {
-                    if (livingHeighbor == 2 || livingHeighbor == 2)
+                    if (livingHeighbor == 2 || livingHeighbor == 3)
                         masTemp[_x, _y] = 1;
                     else masTemp[_x, _y] = 0;
                 }
-
                 //Console.Write(livingHeighbor);
             }
-
         }
+
+        //Подсчет живых клеток
+        livedPoints = 0;
+        foreach (int i in mas)
+        {
+            if (i == 1) livedPoints++;
+        }
+        if (livedPoints == 0) Console.WriteLine("Все клетки пусты. Игра окончена");
 
         mas = masTemp;
     }
@@ -148,15 +164,26 @@ internal class Program
         return livingHeighbor;
     }
 
-    static void RenderLife(int[,] mas)
+    private static void SetTimer()
     {
+        // Create a timer with a two second interval.
+        aTimer = new System.Timers.Timer(timerCouldown);
+        // Hook up the Elapsed event for the timer. 
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+    }
 
+    private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                          e.SignalTime);
     }
 
     static bool AskRestartGame()
     {
         Console.WriteLine("\nПерезапустить игру?\nY/N");
-        if (Console.ReadLine() == "Y")
+        if (Console.ReadLine() == "Y" || Console.ReadLine() == "y")
         {
             //Game(); //useless
             return true;
